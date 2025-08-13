530c579c9a432aa78499759aa11cd6a53c904ba7
Isaac Monson <isaacmonson@Isaacs-MacBook-Air.local>
2025-08-13 11:13:49 -0600

    redo

--- src/components/NewTech.jsx
+++ src/components/NewTech.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import CodeEffect from './CodeEffect';
 import { techSectionsSegments } from './array';
-import '../styles/tech-card.scss';
 
-// logos
 import htmlLogo from '../assets/HTML.svg';
 import javaLogo from '../assets/java.svg';
 import reactLogo from '../assets/react.svg';
